731a9e6a8799f2436d02929e21773a504cea207a
Mirko Klemm <mirko@cm-klemm.de>
2015-01-30 01:58:33 +0100

    Organized imports

--- src/main/java/com/kscs/util/plugins/xjc/episode/PluginImpl.java
+++ src/main/java/com/kscs/util/plugins/xjc/episode/PluginImpl.java
@@ -40,6 +40,15 @@
 
 package com.kscs.util.plugins.xjc.episode;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import com.sun.codemodel.fmt.JPropertyFile;
 import com.sun.codemodel.fmt.JTextFile;
 import com.sun.tools.xjc.BadCommandLineException;
@@ -56,15 +65,29 @@ import com.sun.xml.bind.v2.schemagen.episode.Bindings;
 import com.sun.xml.bind.v2.schemagen.episode.SchemaBindings;
 import com.sun.xml.txw2.TXW;
 import com.sun.xml.txw2.output.StreamSerializer;
-import com.sun.xml.xsom.*;
+import com.sun.xml.xsom.XSAnnotation;
+import com.sun.xml.xsom.XSAttGroupDecl;
+import com.sun.xml.xsom.XSAttributeDecl;
+import com.sun.xml.xsom.XSAttributeUse;
+import com.sun.xml.xsom.XSComplexType;
+import com.sun.xml.xsom.XSComponent;
+import com.sun.xml.xsom.XSContentType;
+import com.sun.xml.xsom.XSDeclaration;
+import com.sun.xml.xsom.XSElementDecl;
+import com.sun.xml.xsom.XSFacet;
+import com.sun.xml.xsom.XSIdentityConstraint;
+import com.sun.xml.xsom.XSModelGroup;
+import com.sun.xml.xsom.XSModelGroupDecl;
+import com.sun.xml.xsom.XSNotation;
+import com.sun.xml.xsom.XSParticle;
+import com.sun.xml.xsom.XSSchema;
+import com.sun.xml.xsom.XSSimpleType;
+import com.sun.xml.xsom.XSWildcard;
+import com.sun.xml.xsom.XSXPath;
 import com.sun.xml.xsom.visitor.XSFunction;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
-
-import java.io.IOException;
-import java.io.StringWriter;
-import java.util.*;
 
 /**
  * Creates the episode file,
